eb5a4cf9aa7677ffa45870558d1dcb8f57ae4da6
ShinjiX-Web <npanares4@gmail.com>
2023-04-11 23:01:09 +0800

    Version_1.0

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function SignIn()
   return (
     <div className="home-chat">
       <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
+      <p>Be respectful to others or you will be banned for life!</p>
     </div>
   )
 
